c46c6514cc293d2bef8d3764bc8e2c0223816491
Goocal <techfrench48@gmail.com>
2024-08-09 06:43:46 +0200

    Add files via upload

---
+++ helloworld.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+int main(){
+  std::cout << "There was a man called john " << std::endl;
+  std::cout << "He was 80 years old " << std::endl;
+  std::cout << "Lived in London, he liked " << std::endl;
+  std::cout << "The name John but he don't like " << std::endl;
+  std::cout << "to be 80 years old " << std::endl;
+}
